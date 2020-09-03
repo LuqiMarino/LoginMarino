85ef787c438342b59f9804a8c2402816ed7863aa
LuqiMarino <lucasmarino96@hotmail.com>
2020-09-03 20:36:40 -0300

    router

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router){}
+
+  public user:string;
+  public pw:string;
 
   ngOnInit(): void {
+  }
+
+  public Loguearse():void{
+    if (this.user == "admin@mail.com" && this.pw == "1234")
+      this.route.navigate(['bienvenida']);
+    else
+      this.route.navigate(['**']);
   }
 
 }
