b4cd01ca2d7098c16f1a1d6bdea76b331c30be16
LuqiMarino <lucasmarino96@hotmail.com>
2020-09-24 21:48:59 -0300

    chau environment

--- src/app/componentes/paises-regiones/paises-regiones.component.ts
+++ src/app/componentes/paises-regiones/paises-regiones.component.ts
@@ -16,7 +16,7 @@ export class PaisesRegionesComponent implements OnInit {
 
   ngOnInit(): void {  
     this.regionSeleccionada = "Europa";
-    this.url = environment.urlApiPaisesEuropa;
+    this.url = "https://restcountries.eu/rest/v2/region/europe";
     this.SeleccionarRegion(1);  
   }
 
@@ -24,15 +24,15 @@ export class PaisesRegionesComponent implements OnInit {
     switch(opc){
       case 1:
         this.regionSeleccionada = "Europa";
-        this.url = environment.urlApiPaisesEuropa;
+        this.url = "https://restcountries.eu/rest/v2/region/europe";
         break;
       case 2:
         this.regionSeleccionada = "Asia";
-        this.url = environment.urlApiPaisesAsia;
+        this.url = "https://restcountries.eu/rest/v2/region/asia";
         break;
       case 3:
         this.regionSeleccionada = "Africa";
-        this.url = environment.urlApiPaisesAfrica;
+        this.url = "https://restcountries.eu/rest/v2/region/africa";
         break;
     }
     this.emitirDevolverRegion.emit(this.regionSeleccionada);
